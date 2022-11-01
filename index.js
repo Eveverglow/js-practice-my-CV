3a730daf268cc2dd6d3ac6a02a9269e9fc6b0dfb
github_Eveverglow <danko199915@gmail.com>
2022-11-01 22:26:33 +0300

    create field for game

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cvName = ['VICTORIA DANKO']
 var textPosition = 0; 
 var speed = 100;
+
 typewriter = () => {
   document.querySelector("#cv-name").innerHTML = cvName[0].substring(0, textPosition) + '<span>\u25AE</span>';
   
@@ -13,8 +14,21 @@ window.addEventListener("load", typewriter)
 var startGame = document.querySelector("#start-game")
 var runGame = document.querySelector(".game-field")
 var resume = document.querySelector(".container")
+var loading = ['Loading...']
+var textProcessor = 0
+var speedForLoader = 200
+
+
+loader = () => {
+  document.querySelector(".loader-text").innerHTML = loading[0].substring(0, textProcessor) + '<span>\u25AE</span>';
+  
+  if(textProcessor++ != loading[0].length)
+    setTimeout(loader, speedForLoader);
+}
 
 startGame.addEventListener('click', function (){
    runGame.classList.toggle("open");
    resume.classList.toggle ("close");
+   loader()
+   
 })
